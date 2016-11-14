3082cdddf5e5d8dacd65bbfa528c10400bb1f6e0
jmazzoni <jacopo.mazzoni@gmail.com>
2016-11-14 13:30:39 -0600

    initial upload

---
+++ BearlyInTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BearlyInTouch
+//
+//  Created by super on 11/14/16.
+//  Copyright © 2016 BearlyInTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
