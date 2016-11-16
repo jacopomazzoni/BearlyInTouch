3f97c80d5c038e5231bdfdf521133a92d0f66344
jmazzoni <jacopo.mazzoni@gmail.com>
2016-11-16 11:52:51 -0600

    in class commit

---
+++ BearlyInTouch/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  archat
+//
+//  Created by Labuser on 11/6/16.
+//  Copyright © 2016 chat. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+    var fromId: String?
+    var text: String?
+    var timestamp: NSNumber?
+    var toId: String?
+}
---
+++ BearlyInTouch/user.swift
@@ -0,0 +1,16 @@
+//
+//  user.swift
+//  archat
+//
+//  Created by Labuser on 11/6/16.
+//  Copyright © 2016 chat. All rights reserved.
+//
+
+import UIKit
+
+class user: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+
+}
