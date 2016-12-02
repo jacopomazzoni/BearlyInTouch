4cfa3dfc3f46c0191806b4d4741b66741f14094b
jmazzoni <jacopo.mazzoni@gmail.com>
2016-12-02 13:50:33 -0600

    improvements on the settings page

--- BearlyInTouch/SettingsController.swift
+++ BearlyInTouch/SettingsController.swift
@@ -18,6 +18,7 @@ import FirebaseDatabase
 class SettingsController: UITableViewController {
     
     var settingsView = [UITableViewCell]()
+    var userEmail = String()
     
     let profileImageView : UIImageView = {
         let imageView = UIImageView()
@@ -28,51 +29,31 @@ class SettingsController: UITableViewController {
     
     override func viewDidLoad() {
         
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        super.viewDidLoad()
+
         // Add navbar Title
         navigationItem.title = "Settings"
-        self.populateSV()
+        
+        // Load User Email and array of cells
+        self.fetchUserAndPopulate()
+        
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return settingsView.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        //FIXME : SAFEUNWRAP?
         return ( settingsView[indexPath.row] )
-        
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
-        if (settingsView[indexPath.row].reuseIdentifier == "spacer")
-        {
-            return 30
-        }
-        else
-        {
-            return 60
-        }
-        
-    }
     
-    lazy var test: UIButton = {
-        let button = UIButton(type: .System)
-        //button.backgroundColor = UIColor.whiteColor()
-        //UIColor(r: 244, g: 72, b: 66 )
-        button.setTitle("change", forState: .Normal)
-        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        //button.translatesAutoresizingMaskIntoConstraints = false
-        //button.titleLabel?.font = UIFont.boldSystemFontOfSize(16)
-        //button.layer.cornerRadius = 5
-        //button.addTarget(self, action: #selector(handleLoginRegister), forControlEvents: .TouchUpInside)
-        //button.setTitleColor(theColor, forState: .Normal)
-        return button
-    }()
+        return (settingsView[indexPath.row].reuseIdentifier == "spacer" ? 30 : 60)
+    }
 
     
     func populateSV(){
@@ -95,14 +76,13 @@ class SettingsController: UITableViewController {
             cell.textLabel?.text = "Full Name"
             cell.detailTextLabel?.text = "Load user Name here"
             cell.textLabel?.textAlignment = .Left
-            
-            cell.addSubview(test)
+
             return cell }())
         
         settingsView.append( {
             let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "email")
             cell.textLabel?.text = "Email"
-            cell.detailTextLabel?.text = "Load user Email here"
+            cell.detailTextLabel?.text = self.userEmail
             cell.textLabel?.textAlignment = .Left
             return cell }())
         
@@ -138,7 +118,9 @@ class SettingsController: UITableViewController {
         settingsView.append( {
             let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "logout")
             cell.textLabel?.text = "Logout"
-            cell.detailTextLabel?.text = "email"
+            cell.detailTextLabel?.text = self.userEmail
+            print(self.userEmail)
+            print ("Jacopo")
             cell.textLabel?.textAlignment = .Center
             return cell }())
     }
@@ -159,6 +141,8 @@ class SettingsController: UITableViewController {
         case 4:
             print(" tapped on \(id)")
         case 5:
+            print(" tapped on \(id)")
+        case 7:
             print(" tapped on \(id)")
         case 9:
             self.handleLogout()
@@ -181,64 +165,23 @@ class SettingsController: UITableViewController {
         presentViewController(loginController, animated: true, completion: nil)
     }
     
+    func fetchUserAndPopulate()
+    {
+        var value : String = ""
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else
+        { return }
+        
+        FIRDatabase.database().reference().child("users").child(uid).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+            if let dictionary = snapshot.value as? [String : AnyObject] {
+                value = (dictionary["email"] as? String)!
+                self.userEmail = value
+                self.populateSV()
+                self.tableView.reloadData()
+            }
+            }, withCancelBlock: nil )
+    }
+   
     
 }
 
 
-
-class CustomCell: UITableViewCell {
-    var cellButton: UIButton!
-    var cellLabel: UILabel!
-    
-    init(frame: CGRect, title: String) {
-        super.init(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
-        
-        cellLabel = UILabel(frame: CGRectMake(self.frame.width - 100, 10, 100.0, 40))
-        cellLabel.textColor = UIColor.blackColor()
-        //cellLabel.font = //set font here
-            
-        cellButton = UIButton(frame: CGRectMake(200, 10, 100, 30))
-        cellButton.setTitle(title, forState: UIControlState.Normal)
-        cellButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
-        
-        addSubview(cellLabel)
-        addSubview(cellButton)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
-}
-
-/*
- let inputsContainerView : UIView = {
- let view = UIView()
- view.backgroundColor = UIColor.whiteColor()
- view.translatesAutoresizingMaskIntoConstraints = false
- view.layer.cornerRadius = 10
- view.layer.masksToBounds = true
- return view
- }()
- 
- let profileImageView :UIImageView = {
- let imageView = UIImageView()
- imageView.image = UIImage(named: "bear")
- imageView.translatesAutoresizingMaskIntoConstraints = false
- return imageView
- }()
- lazy var loginRegisterButton: UIButton = {
- let button = UIButton(type: .System)
- button.backgroundColor = UIColor.whiteColor()
- //UIColor(r: 244, g: 72, b: 66 )
- button.setTitle("Register", forState: .Normal)
- button.translatesAutoresizingMaskIntoConstraints = false
- button.titleLabel?.font = UIFont.boldSystemFontOfSize(16)
- button.layer.cornerRadius = 5
- button.addTarget(self, action: #selector(handleLoginRegister), forControlEvents: .TouchUpInside)
- button.setTitleColor(theColor, forState: .Normal)
- return button
- }()*/
