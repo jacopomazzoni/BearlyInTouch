9986870ec87a861dcf855d3e6150fbd5c6da849a
Carlos Gonzalez <cgonzalezmendoza@wustl.ed>
2016-12-04 22:01:19 -0600

    Match feature finished

---
+++ BearlyInTouch/MatchingController.swift
@@ -0,0 +1,9 @@
+//
+//  MatchingController.swift
+//  BearlyInTouch
+//
+//  Created by Carlos Gonzalez on 12/3/16.
+//  Copyright © 2016 BearlyInTouch. All rights reserved.
+//
+
+import Foundation
